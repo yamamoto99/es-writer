8cb3b795f2a5232132c1d83690f054cb03778fa1
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-06-16 02:20:31 +0900

    feat:comment

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -1,4 +1,5 @@
 terraform {
+	// 必要なプロバイダとそのバージョンを定義
 	required_providers {
 		aws = {
 			source  = "hashicorp/aws"
@@ -6,19 +7,23 @@ terraform {
 		}
 	}
 
+	// 必要なTerraformのバージョンを定義
 	required_version = ">= 1.0.0"
 }
 
 provider "aws" {
+	// AWSの認証情報を設定
 	access_key = "${var.aws_access_key}"
 	secret_key = "${var.aws_secret_key}"
 	region     = "${var.aws_region}"
 }
 
 resource "aws_security_group" "web" {
+	// Webサーバー用のセキュリティグループを定義
 	name = "allow_http"
 	description = "Allow HTTP inbound traffic"
 
+	// HTTPのインバウンドトラフィックを許可
 	ingress {
 		from_port = 80
 		to_port = 80
@@ -26,6 +31,7 @@ resource "aws_security_group" "web" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
+	// すべてのアウトバウンドトラフィックを許可
 	egress {
 		from_port = 0
 		to_port = 0
@@ -35,20 +41,25 @@ resource "aws_security_group" "web" {
 }
 
 resource "aws_instance" "web" {
+	// EC2インスタンスを定義
 	ami = "ami-0f9fe1d9214628296"
 	instance_type = "t2.micro"
 
+	// セキュリティグループを設定
 	vpc_security_group_ids = [aws_security_group.web.id]
 
+	// タグを設定
 	tags = {
 		Name = "progate-aws-app"
 	}
 }
 
 resource "aws_security_group" "rds" {
+	// RDS用のセキュリティグループを定義
 	name = "allow_postgres"
 	description = "Allow PostgreSQL inbound traffic"
 
+	// PostgreSQLのインバウンドトラフィックを許可
 	ingress {
 		from_port = 5432
 		to_port = 5432
@@ -56,6 +67,7 @@ resource "aws_security_group" "rds" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
+	// すべてのアウトバウンドトラフィックを許可
 	egress {
 		from_port = 0
 		to_port = 0
@@ -65,6 +77,7 @@ resource "aws_security_group" "rds" {
 }
 
 resource "aws_db_instance" "main" {
+	// RDSインスタンスを定義
 	identifier = "progate-db"
 	allocated_storage = 20
 	storage_type = "gp2"
@@ -73,9 +86,12 @@ resource "aws_db_instance" "main" {
 	instance_class = "db.t3.micro"
 	password = "${var.rds_pass}"
 	username = "${var.rds_username}"
+
+	// セキュリティグループを設定
 	vpc_security_group_ids = [aws_security_group.rds.id]
 	skip_final_snapshot = true
 
+	// タグを設定
 	tags = {
 		Name = "progate-db"
 	}
