e6d3471095a854d25d83af4ede594612143ffde8
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-06-16 17:18:52 +0900

    なんとかなれ

--- backend/go.mod
+++ backend/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.27.18
 	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.38.5
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/net v0.26.0
 )
--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -47,6 +48,10 @@ func main() {
 	}
 	defer db.Close()
 
+	err = godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
 	cognitoRegion = os.Getenv("COGNITO_REGION")
 	clientId = os.Getenv("COGNITO_CLIENT_ID")
 	jwksURL = os.Getenv("TOKEN_KEY_URL")
