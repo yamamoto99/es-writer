eafde951a2a4699874e21d5c7a859deda0429055
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-06-17 05:13:55 +0900

    fix: フロントエンドからAPIを呼び出せない問題を修正

--- es-writer-extension/popup/index.tsx
+++ es-writer-extension/popup/index.tsx
@@ -1,15 +1,10 @@
 import React from "react"
-import indexContents from "../contents/index"
+import indexContents from "./indexContents"
 
 function IndexPopup() {
-  const handleButtonClick = () => {
-    console.log("Button clicked") // ボタンがクリックされたことを確認するためのログ
-    indexContents()
-  }
-
   return (
     <div style={{ width: '150px', height: '75px' }}>
-      <button onClick={handleButtonClick}>回答生成</button>
+      <button onClick={indexContents}>回答生成</button>
     </div>
   )
 }
