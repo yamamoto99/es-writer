5d4669ba683b53a2dc979d4dacc6e2c589299b36
Yuto <sasayu8190@gmail.com>
2024-06-19 14:34:44 +0900

    fix: change file path,けど動かない

--- es-writer-extension/src/popup/openProfileForm.tsx
+++ es-writer-extension/src/popup/openProfileForm.tsx
@@ -1,7 +1,8 @@
 /// <reference types="chrome"/>
 
 function openProfileForm() {
-  chrome.tabs.create({ url: chrome.runtime.getURL("src/pages/profile.html") });
+  console.log("openProfile called")
+  chrome.tabs.create({ url: chrome.runtime.getURL("../pages/profile.html") });
 }
 
 export default openProfileForm;
