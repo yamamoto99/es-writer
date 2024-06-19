f61439c5dde9faf3d8d700541a129be3261fb3e4
Yuto <sasayu8190@gmail.com>
2024-06-19 14:03:05 +0900

    feat: add styles

--- es-writer-extension/src/popup/openProfileForm.tsx
+++ es-writer-extension/src/popup/openProfileForm.tsx
@@ -1,7 +1,7 @@
 /// <reference types="chrome"/>
 
 function openProfileForm() {
-  chrome.tabs.create({ url: chrome.runtime.getURL('src/pages/profile.html') });
+  chrome.tabs.create({ url: chrome.runtime.getURL("src/pages/profile.html") });
 }
 
 export default openProfileForm;
