0f994235a2d113c51af4ff70f474f1506852e9cc
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-07-02 16:11:59 +0900

    fix: add api endpoint

--- es-writer-extension/src/components/Profile.tsx
+++ es-writer-extension/src/components/Profile.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react"
 
+import { api_endpoint } from "../contents/index"
+
 const Profile = () => {
   const [profile, setProfile] = useState({
     bio: "",
