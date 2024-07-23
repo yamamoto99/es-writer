8afe9f52bb7facc595318803786d2b85c3ee4bd4
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-07-23 12:50:42 +0900

    fix: Fixed to retry if migration fails

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,11 @@
 set -e
 
 # マイグレーションを実行
-/app/migrate/main
+#　マイグレーションが成功しなかった場合、再度実行する
+until /app/migrate/main; do
+  echo "マイグレーションに失敗しました。再試行します..."
+  sleep 1
+done
 
 # APIサーバーを起動
 exec /app/main
