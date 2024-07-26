9387b4a4d7ba4c19968900c8d1f7d91afb4d2b82
Yuto <sasayu8190@gmail.com>
2024-07-26 20:27:38 +0900

    refactor: フォーマッター

--- es-writer-extension/src/popup/routes/logOut.tsx
+++ es-writer-extension/src/popup/routes/logOut.tsx
@@ -1,37 +1,38 @@
-import React from "react";
+import React from "react"
+import { useNavigate } from "react-router-dom"
+
 import { useStorage } from "@plasmohq/storage/hook"
-import { useNavigate } from "react-router-dom";
-import { api_endpoint } from "../../contents";
+
+import { api_endpoint } from "../../contents"
 
 function LogOut() {
-  const navigate = useNavigate();
-  const [loginState, setLoginState] = useStorage<string>("loginState");
+  const navigate = useNavigate()
+  const [loginState, setLoginState] = useStorage<string>("loginState")
 
   const handleLogout = async () => {
     try {
       const response = await fetch(api_endpoint + "/auth/logout", {
         method: "POST",
-        credentials: "include",
-      });
+        credentials: "include"
+      })
       if (response.ok) {
-        await setLoginState("not-logged-in");
-        navigate("/");
+        await setLoginState("not-logged-in")
+        navigate("/")
       } else {
-        console.error("Logout failed");
+        console.error("Logout failed")
       }
     } catch (error) {
-      console.error("An error occurred during logout", error);
+      console.error("An error occurred during logout", error)
     }
-  };
+  }
 
   return (
-    <button 
+    <button
       className="block mx-auto bg-gray-500 hover:bg-gray-700 text-white rounded-md w-24 h-8 p-2 mt-1 mb-4"
-      onClick={handleLogout}
-    >
+      onClick={handleLogout}>
       ログアウト
     </button>
-  );
+  )
 }
 
-export default LogOut;
+export default LogOut
